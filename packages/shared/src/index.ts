ba329ba038e9b7b4123f06dce6c60e31c5661c34
Debbie MidstCodes <trentbuzzmedia@gmail.com>
2026-08-05 14:07:00 +0000

    fix: import MintStatus from nft types not common types in shared barrel

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,4 +1,5 @@
 export * from './types';
 export * from './constants';
 export * from './utils';
-export type { MintStatus, EventType } from './types/common';
+export type { MintStatus } from './types/nft';
+export type { EventType } from './types/common';
