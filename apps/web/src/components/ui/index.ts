eeca5d70e6e12dd958c4ebaf0c713e33ce291c96
Debbie MidstCodes <trentbuzzmedia@gmail.com>
2026-08-05 12:09:11 +0000

    feat: export ErrorBoundary from ui barrel file

--- apps/web/src/components/ui/index.ts
+++ apps/web/src/components/ui/index.ts
@@ -3,3 +3,4 @@ export { default as StatCard } from './StatCard';
 export { default as ActivityItem } from './ActivityItem';
 export { default as EmptyState } from './EmptyState';
 export { default as LoadingSkeleton } from './LoadingSkeleton';
+export { default as ErrorBoundary } from './ErrorBoundary';
